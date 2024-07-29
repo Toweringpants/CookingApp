80a5c75ceaa4a3afa2e721ebec65b24de758b249
mattechenique <mme21a@fsu.edu>
2024-07-28 22:28:50 -0400

    adding back button functionality

--- app/src/main/java/com/example/cookingapp/SandwichesFragment.java
+++ app/src/main/java/com/example/cookingapp/SandwichesFragment.java
@@ -1,20 +1,26 @@
 package com.example.cookingapp;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class SandwichesFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecipeAdapter adapter;
     private long categoryId;
+    private NavController navController;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,11 +32,19 @@ public class SandwichesFragment extends Fragment {
 
         categoryId = 2;
 
+        navController = NavHostFragment.findNavController(this);
+
         new Thread(() -> {
             List<Recipe> recipes = RecipeRepository.getInstance(getContext()).getRecipesByCategory(categoryId);
             getActivity().runOnUiThread(() -> adapter.submitList(recipes));
         }).start();
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.buttonBack).setOnClickListener(v -> navController.navigate(R.id.action_sandwichesFragment_to_mainFragment));
     }
 }
--- app/src/main/java/com/example/cookingapp/WorldFragment.java
+++ app/src/main/java/com/example/cookingapp/WorldFragment.java
@@ -1,20 +1,26 @@
 package com.example.cookingapp;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
 public class WorldFragment extends Fragment {
     private RecyclerView recyclerView;
     private RecipeAdapter adapter;
     private long categoryId;
+    private NavController navController;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,6 +32,8 @@ public class WorldFragment extends Fragment {
 
         categoryId = 4;
 
+        navController = NavHostFragment.findNavController(this);
+
         new Thread(() -> {
             List<Recipe> recipes = RecipeRepository.getInstance(getContext()).getRecipesByCategory(categoryId);
             getActivity().runOnUiThread(() -> adapter.submitList(recipes));
@@ -33,4 +41,11 @@ public class WorldFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.buttonBack).setOnClickListener(v -> navController.navigate(R.id.action_worldFragment_to_mainFragment));
+    }
 }
+
